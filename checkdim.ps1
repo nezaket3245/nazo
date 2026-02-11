2f63ae7a55a080bf35c37532580e66b1cb7d10af
Ibo <ibo@example.com>
2026-02-11 17:26:52 +0300

    Site dosyalari koke tasindi - Cloudflare Pages uyumlu yapi

--- checkdim.ps1
+++
@@ -1,7 +0,0 @@
-Add-Type -AssemblyName System.Drawing
-$bp = "c:\Users\ibo\Desktop\12\egepenakcayapi.com\wp-content\uploads\2022\05"
-Get-ChildItem "$bp\kedi-sineklik-p*.jpg" | ForEach-Object {
-    $i = [System.Drawing.Image]::FromFile($_.FullName)
-    Write-Host "$($_.Name): $($i.Width)x$($i.Height) ($([math]::Round($_.Length/1024))KB)"
-    $i.Dispose()
-}
